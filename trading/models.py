53aaa8c107ddc7febfab7ba9c24157f8f7dd75d2
air <talko.alexander@gmail.com>
2024-10-28 01:00:23 +0400

    add models

--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,139 @@
+from decimal import Decimal
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class BaseModel(models.Model):
+    """Базовая модель с полями создания и изменения"""
+    objects = models.Manager()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="время последнего изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Contacts(BaseModel):
+    """Модель контактов"""
+    email = models.EmailField(max_length=100, verbose_name='email')
+    country = models.CharField(max_length=100, verbose_name='страна')
+    city = models.CharField(max_length=100, verbose_name='город')
+    street = models.CharField(max_length=100, verbose_name='улица')
+    house_number = models.CharField(max_length=100, verbose_name='номер дома')
+
+    def __str__(self):
+        return f'{self.email}, {self.country}, {self.city}, {self.street}, {self.house_number}'
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
+
+
+class Products(BaseModel):
+    """Модель продуктов"""
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    model = models.CharField(max_length=100, verbose_name='модель')
+    release_date = models.DateField(auto_now_add=False, verbose_name='дата выпуска', help_text='ДД.ММ.ГГГГ')
+
+    def __str__(self):
+        return '%s, %s, %s' % (self.name, self.model, self.release_date)
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Factory(BaseModel):
+    """Модель производителя"""
+    name = models.CharField(max_length=100)
+    contacts = models.OneToOneField(Contacts, on_delete=models.CASCADE, related_name='address')
+    products = models.ManyToManyField(Products)
+    debt = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=Decimal('0.00'),
+        verbose_name='задолженность',
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return f'Производитель: {self.name}, контакты: {self.contacts}'
+
+    class Meta:
+        verbose_name = 'производитель'
+        verbose_name_plural = 'производители'
+
+
+class RetailNetwork(BaseModel):
+    """Модель розничной сети"""
+    name = models.CharField(max_length=100)
+    contacts = models.OneToOneField(Contacts, on_delete=models.CASCADE, related_name='retail_network')
+    products = models.ManyToManyField(Products)
+    provider_factory = models.ForeignKey(
+        Factory,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name='supplied_retail_networks'
+    )
+    debt = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=Decimal('0.00'),
+        verbose_name='задолженность',
+        **NULLABLE
+    )
+
+    def __str__(self):
+        return f'Розничная сеть: {self.name}, контакты: {self.contacts}'
+
+    class Meta:
+        verbose_name = 'розничная сеть'
+        verbose_name_plural = 'розничные сети'
+
+    def get_provider(self):
+        """Идентификация поставщика"""
+        if self.provider_factory:
+            return self.provider_factory.name
+        return "Нет поставщика"
+
+
+class Trader(BaseModel):
+    """Модель индивидуальный предприниматель"""
+    name = models.CharField(max_length=100)
+    contacts = models.OneToOneField(Contacts, on_delete=models.CASCADE, related_name='trader')
+    products = models.ManyToManyField(Products)
+    provider_factory = models.ForeignKey(
+        Factory,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name='provider_for_traders'
+    )
+    provider_retail_network = models.ForeignKey(
+        RetailNetwork,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name='provider_for_traders'
+    )
+    debt = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=Decimal('0.00'),
+        verbose_name='задолженность'
+    )
+
+    def __str__(self):
+        return f'Индивидуальный предприниматель: {self.name}, контакты: {self.contacts}'
+
+    class Meta:
+        verbose_name = 'индивидуальный предприниматель'
+        verbose_name_plural = 'индивидуальные предприниматели'
+
+    def get_provider(self):
+        """Идентификация поставщика"""
+        if self.provider_factory:
+            return self.provider_factory.name
+        elif self.provider_retail_network:
+            return self.provider_retail_network.name
+        return 'Нет поставщика'
