99b58f81933b82e70c6c83c133039fef32853a9c
air <talko.alexander@gmail.com>
2024-10-28 01:00:10 +0400

    add admin.py

--- trading/admin.py
+++ trading/admin.py
@@ -1,3 +1,87 @@
 from django.contrib import admin
+from django.core.checks import messages
+from trading.models import Contacts, Products, Factory, RetailNetwork, Trader
 
-# Register your models here.
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('email', 'country', 'city', 'street', 'house_number',)
+    list_filter = ('country', 'city')
+    search_fields = ('email', 'country', 'city',)
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'release_date',)
+    list_filter = ('name', 'model', 'release_date',)
+    search_fields = ('name', 'model', 'release_date',)
+
+
+class CityFilter(admin.SimpleListFilter):
+    """Фильтр для отображения города"""
+    title = 'Город'
+    parameter_name = 'contacts__city'
+
+    def lookups(self, request, model_admin):
+        # Возвращает список уникальных городов для фильтрации
+        cities = set(Contacts.objects.values_list('city', flat=True))
+        return [(city, city) for city in cities]
+
+    def queryset(self, request, queryset):
+        # Фильтрует по выбранному городу
+        if self.value():
+            return queryset.filter(contacts__city=self.value())
+        return queryset
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contacts', 'created_at',)
+    list_filter = ('name', 'created_at', CityFilter)
+    search_fields = ('name',)
+    actions = ['set_null']
+
+    @admin.action(description='Очистка задолженности покупателей у выбранных объектов')
+    def set_null(self, request, queryset):
+        queryset.update(debt=None)
+        self.message_user(request, 'Задолженности успешно очищены.', messages.WARNING)
+
+
+@admin.register(RetailNetwork)
+class RetailNetworkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contacts', 'created_at', 'get_provider',)
+    list_filter = ('name', 'created_at', CityFilter)
+    search_fields = ('name',)
+    actions = ['set_null']
+
+    @admin.display(description='Поставщик')
+    def get_provider(self, obj):
+        if obj.provider_factory:
+            return obj.provider_factory.name
+        return 'Нет поставщика'
+
+    @admin.action(description='Очистка задолженности перед поставщиком у выбранных объектов')
+    def set_null(self, request, queryset):
+        queryset.update(debt=None)
+        self.message_user(request, 'Задолженности успешно очищены.', messages.WARNING)
+
+
+@admin.register(Trader)
+class TraderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contacts', 'created_at', 'get_provider',)
+    list_filter = ('name', 'created_at', CityFilter)
+    search_fields = ('name',)
+    actions = ['set_null']
+
+    @admin.display(description='Поставщик')
+    def get_provider(self, obj):
+        if obj.provider_factory:
+            return obj.provider_factory.name
+        elif obj.provider_retail_network:
+            return obj.provider_retail_network.name
+        return 'Нет поставщика'
+
+    @admin.action(description='Очистка задолженности перед поставщиком у выбранных объектов')
+    def set_null(self, request, queryset):
+        queryset.update(debt=None)
+        self.message_user(request, 'Задолженности успешно очищены.', messages.WARNING)
